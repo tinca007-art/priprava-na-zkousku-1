b5a38d0abaa9ba7390368352df6677bfe70e00c7
“Kristina <tinca007@seznam.cz>
2026-05-29 19:43:36 +0200

    úkol 4

--- vite.config.js
+++ vite.config.js
@@ -10,8 +10,8 @@ export default defineConfig({
   // --- DAY 1 ---
   // root: './Task_1',
   // root: './Task_2',
-   root: './Task_3',
-  // root: './Task_4',
+  // root: './Task_3',
+   root: './Task_4',
   // root: './Task_5',
 
 })
