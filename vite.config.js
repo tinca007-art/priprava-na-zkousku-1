fda7bd6e81d05ccc44e94202e4f31f3e90b60e59
“Kristina <tinca007@seznam.cz>
2026-05-29 19:20:30 +0200

    úkol 2

--- vite.config.js
+++ vite.config.js
@@ -8,8 +8,8 @@ export default defineConfig({
   // Replace with the path to your folder containing index.html
 
   // --- DAY 1 ---
-  root: './Task_1',
-  // root: './Task_2',
+  // root: './Task_1',
+   root: './Task_2',
   // root: './Task_3',
   // root: './Task_4',
   // root: './Task_5',
